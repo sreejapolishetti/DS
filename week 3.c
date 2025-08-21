cfef358f99c82145ed5b9614ce6932da47a982fd
sreejapolishetti <sreejapolishetti@gmail.com>
2025-08-21 12:26:16 +0530

    Add files via upload

---
+++ week 3.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#define MAX 100
+char stack[MAX];
+int top = -1;
+//push function
+void push(char c)
+{
+	if(top < MAX-1)
+	{
+		stack[++top] = c;
+	}
+}
+//pop function
+char pop()
+{
+	if (top >= 0)
+   {
+   	return stack[top--];
+   }
+   return -1;
+}
+// peek fumction
+char peek()
+{
+	if (top>=0)
+	{
+		return stack[top];
+	}
+	return -1;
+}
+//function to define operator precedence
+int precedence(char op)
+{
+	switch (op)
+	{
+		case '*':
+		case '/':
+		case '%': return 2;
+		case '+':
+		case '-': return 1;
+		default: return 0;	
+	}
+}
+//functuion to check if character is operator
+int isoperator(char c)
+{
+	return (c == '+'|| c == '-'|| c == '*'|| c == '/'|| c == '%');
+	
+}
+//Infix to postfix conversation 
+void infixtopostfix(char infix[], char postfix[])
+{
+	int i,k=0;
+	char symbol;
+	for (i =0; i<strlen(infix); i++)
+	{
+		symbol = infix[i];
+		// operand ? directly add to postfix
+		if (isalnum(symbol))
+		{
+			postfix[k++]=symbol;
+		}
+		// left paranthesis ? push to stack
+		else if (symbol=='(')
+		{
+			push(symbol);
+			
+		}
+		// right parentisis ? pop until'('
+		else if (symbol ==')')
+		{
+			while (top != -1 && peek() != '(')
+			{
+				postfix[k++]=pop();
+			}
+			pop(); // remove '('
+		}
+		//operator
+		else if (isoperator(symbol))
+		{
+			while (top != -1 && precedence(peek()) >= precedence(symbol))
+			{
+				postfix[k++] = pop();
+			}
+			push(symbol);
+		}
+	}
+	
+	while (top != -1)
+	{
+		postfix[k++]=pop();
+	}	
+	
+	postfix[k] ='\0';//null terminate string
+}
+int main()
+{
+	char infix[MAX],postfix[MAX];
+	printf("Enter an infix expression:");
+	scanf("%s",infix);
+	infixtopostfix(infix,postfix);
+	printf("Postfix expression:%s\n",postfix);
+	return 0;
+} 
